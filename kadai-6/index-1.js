1dd9f90f63f428b8b3fa550c079b5e9cefa1e1cc
Kamata Ryo <mugil.cephalus+github@gmail.com>
2019-11-27 04:51:42 +0000

    課題6-1を更新

--- kadai-6/index-1.js
+++ kadai-6/index-1.js
@@ -7,5 +7,6 @@ const obj6 = { property: undefined }
 const obj7 = {a: "aaa", b: { c: "ccc", d: undefined } }
 
 console.log(JSON.stringify(obj1))
+console.log(JSON.stringify(obj2))
 console.log(JSON.stringify(obj5))
 console.log(JSON.stringify(obj7))
