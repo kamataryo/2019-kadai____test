aa51db4f817b349a14cfd929524a3a565959d8ac
Kamata Ryo <mugil.cephalus+github@gmail.com>
2020-01-15 05:11:46 +0000

    課題11を追加

--- kadai-11/index.js
+++ kadai-11/index.js
@@ -2,8 +2,19 @@ const http = require('http')
 
 
 const server = http.createServer((req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.write('Hello')
+            const num = Math.random()
+            res.setHeader('Access-Control-Allow-Origin', '*');
+        
+            if(num < 0.5) {
+                res.write('吉です')
+            } else if(num < 0.8) {
+                res.write('中吉です')
+            } else if (num < 0.95) {
+                res.write('大吉です')
+            } else {
+                res.write('きょうです')
+            }
+
     res.end()
 })
 server.listen(4000)
